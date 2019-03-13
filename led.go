70c50a931d6fe0b5bfef70c07b21386535915737
Marcin Lewandowski <marcinlewy22@gmail.com>
2019-03-13 21:54:07 +0100

    Refactor

--- led.go
+++ led.go
@@ -6,29 +6,29 @@ import (
     "strconv"
 )
 
-var VALUE_PATH string = "/sys/class/gpio/gpio21/value"
+var VALUE_PATH string = "/sys/class/gpio/gpio%i/value"
+
+func readPinValue(pin_number int) -> int, error {
+    pin_value_path := fmt.Spritf(VALUE_PATH, pin_number)
+    f, err := os.OpenFile(pin_value_path, os.O_RDONLY, 0755)
+    if err != nil {
+        return 0, error("Cannot access value file.")
+    }
+    defer f.Close()
+    file_content := make([]byte, 1)
+    bytes_read, err := f.Read(file_content)
+    if err != nil || bytes_read != 1 {
+        return 0, "Error reading value."
+    }
+    led_state, err := strconv.ParseInt(string(file_content), 10, 8)
+    if err != nil {
+        return 0, "Value pseudo-file content invalid."
+    }
+    return led_state, nil
+}
 
 func main() {
-        f, err := os.OpenFile(VALUE_PATH, os.O_RDONLY, 0755)
-        if err != nil {
-            fmt.Println("Przykro mi, błąd przy otwieraniu pliku :(")
-            fmt.Println(err)
-            return
-        }
-        defer f.Close()
-        value := make([]byte, 1)
-        bytesRead, err := f.Read(value)
-        if err != nil || bytesRead != 1 {
-            fmt.Println("Sorry, blad odczytu pliku :c")
-            fmt.Println(err)
-            return
-        }
-        led_state, err := strconv.ParseInt(string(value), 10, 8)
-        if err != nil {
-            fmt.Println("Inwalidzka zawartość pliku :(")
-            return
-        }
-        fmt.Println("Wartość: ", led_state)
+        fmt.Println("Wartość: ", readPinValue(21))
 }
 
 
